e2b7f7a3b1c1def83f1f7adaf4701214203ead30
aminedouiri <aminedouiri2018@gmail.com>
2025-07-04 18:37:40 +0100

    Fix formatting

--- resources.tf
+++ resources.tf
@@ -5,7 +5,7 @@
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
+  region     = var.region
 }
 
 ##################################################################################
@@ -19,7 +19,7 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    
+
   }
 }
 
